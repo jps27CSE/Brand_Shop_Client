2f0111b22380863841e1f3fbe27cc72584dbc44d
jps27CSE <jackpritomsoren@hotmail.com>
2023-10-18 01:48:55 +0600

    Added AddProduct Section

--- src/Components/AddProduct/AddProduct.jsx
+++ src/Components/AddProduct/AddProduct.jsx
@@ -1,5 +1,106 @@
+import addProductPhoto from "../../assets/addproduct.png";
+
 const AddProduct = () => {
-  return <div></div>;
+  return (
+    <div className="max-w-7xl mx-auto">
+      <div>
+        <h1 className="text-4xl font-bold text-center mt-5 mb-2">
+          Add Products
+        </h1>
+      </div>
+      <div className="hero   mx-auto">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="text-center lg:text-left">
+            <img className="w-[600px]" src={addProductPhoto} alt="" />
+          </div>
+          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <form className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Image</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Image Url"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Name Of Product"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Brand Name</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Brand"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Type</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Product Type"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Price</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Price"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Short Description</span>
+                </label>
+                <textarea
+                  type="text"
+                  placeholder="Description"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Rating</span>
+                </label>
+                <input
+                  type="number"
+                  placeholder="Rating"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Add</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AddProduct;
